0758aa4b80b2f4e5f6424303611ddea46d5568a7
marjorie-n <marjorie.ngoupende@gmail.com>
2024-03-23 14:31:41 +0100

    ✨ create MyNFT smart contract  & initialize constructor

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721Enumerable, Ownable {
+    constructor()ERC721Enumerable("MyNFT", "NFT") {
+}
